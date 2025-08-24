7e23cd510b47dbd9fd682529b511a2d292d8cbef
Vansh Kumar Patel <216386028+vansh216@users.noreply.github.com>
2025-08-24 11:37:53 +0530

    getAge.js

--- generate.js
+++ generate.js
@@ -1,53 +1,41 @@
-let url= "https://api.github.com/users/";
-const inputEl= document.getElementById("search");
-const searchBtn=document.getElementById("searchBtn")
-const div=document.querySelector(".card")
+function main(){
+ const dates=document.querySelector (".date")
+  const data=document.querySelector (".data")
+  const sbg= document.querySelector(".secondBackground")
+    sbg.classList.toggle("none")
+    dates.classList.toggle("none")
+    data.classList.toggle("none")
+     
 
-const  generateprofile=  (profile)=>{
-    return(`
-            <div class="intro">
-                <img src="${profile.avatar_url}">
-                <h2>
-                    <ul>
-                        <li>${profile.name}</li>
-                        <li>${profile.email}</li>
-                    </ul>
-                </h2>
-
-                <button id="profile"><a href="${profile.html_url}">Check Profile</a></button>
-            </div>
-            <div class="about">
-                <h1>About</h1>
-                <p>${profile.bio}</p>
-            </div>
-            <div class="follow">
-                <div><h2>Followers</h2><h1>${profile.followers}</h1></div>
-                 <div><h2>Followings</h2><h1>${profile.following}</h1></div>
-                  <div><h2>Post</h2><h1>${profile.public_repos}</h1></div>
-            </div>
-            `)
-}
-
-const getsprofile= async()=>{
-    const username=inputEl.value
-    try{
-
-        const res =await fetch(`${url}${username}`)
-        const a= await res.json();
-        console.log(a)
-            div.innerHTML=generateprofile(a);
-      
-        
-    }catch(error){
-        console.log({error})
-    }
-
-}
-searchBtn.addEventListener("click",()=>{
-    getsprofile()
-    div.classList.remove("none")
-    
+} 
+// start btm
+ document.querySelector(".set").addEventListener("click",(e)=>{
+   e.stopPropagation
+   main()
+   
 })
+// submit btn and date calculate 
+function data(){
+let date= document.getElementById("date").value;
+let currentDate = new Date()
+let dates = new Date(date)
+console.log(currentDate)
+console.log(dates)
+let dateDiff= currentDate-dates;
+const year = (dateDiff/(1000*60*60*24*365))
 
-
-// https://api.github.com/users/defunkt
+const month = (dateDiff/(1000*60*60*24*365))*12
+const day = (dateDiff/(1000*60*60*24*365))*(12*30)
+const hour = (dateDiff/(1000*60*60*24*365))*(12*30*60)
+const second = (dateDiff/(1000*60*60*24*365))*(12*30*60*60)
+let years=document.getElementById("year")
+let months=document.getElementById("month")
+let days=document.getElementById("day")
+let hours=document.getElementById("hour")
+let seconds=document.getElementById("second")
+years.innerHTML=Math.floor(year)+" years"
+months.innerHTML=Math.floor(month)+" months"
+days.innerHTML=Math.floor(day)+" days"
+hours.innerHTML=Math.floor(hour)+" hours"
+seconds.innerHTML=Math.floor( second)+" seconds"
+}
